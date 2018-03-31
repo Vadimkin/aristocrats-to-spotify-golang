682bb19d788253c8719f5beb4249a95231c836e0
Vadim Klimenko <mail@vadimklimenko.com>
2018-03-31 23:34:45 +0300

    Init parser

---
+++ constants.go
@@ -0,0 +1,4 @@
+package main
+
+const AristocratsMain = "http://aristocrats.fm/playlists/"
+//const ARISTOCRATS_MUSIC = "http://aristocrats.fm/playlist-amusic/"
---
+++ types.go
@@ -0,0 +1,6 @@
+package main
+
+
+type Track struct {
+	title, author string
+}
